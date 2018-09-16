021be1a9cb283a8f5078001fb3f0bb726f690580
hazem <hazem@hazems-Mac.local>
2018-09-16 02:08:43 -0700

    hhghghgh

--- Parshot/AppDelegate.swift
+++ Parshot/AppDelegate.swift
@@ -22,6 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let featuredAppsController = FirstAddProductController (collectionViewLayout:layout )
         window?.rootViewController = UINavigationController(rootViewController: featuredAppsController)
        print ("hazemm")
+        print ("hazemm")
+
         return true
     
     }
