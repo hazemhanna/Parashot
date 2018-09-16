0df485e4c87f9e3f68c66d46400472f846a868ae
hossamxp33 <hossamxp33>
2018-09-16 12:16:50 +0200

    شسشس

---
+++ Parshot/FirstMyOrderHeader.swift
@@ -0,0 +1,40 @@
+//
+//  FirstMyOrderHeader.swift
+//  Parshot
+//
+//  Created by hazem on 9/15/18.
+//  Copyright © 2018 hazem. All rights reserved.
+//
+
+import UIKit
+
+
+class FirstMyOrderHeader : FirstSubCategryHeaders   {
+
+
+
+    override func setupViews() {
+  
+        
+        
+ //// this gon't work 
+      //  mainView.isHidden = true
+//        buyBtn.isHidden = true
+//        cartValueLabe.isHidden = true
+//        itemValueLabe.isHidden = true
+//        cartImage.isHidden = true
+//   
+    
+    
+    }
+}
+
+
+
+
+
+
+
+
+
+
