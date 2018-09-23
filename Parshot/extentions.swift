bfb699eef37550394a2146e3d2bb5d036ec2a78c
hossamxp33 <hossamxp33>
2018-09-23 04:35:08 +0200

    pod file and sql lite working

---
+++ Parshot/extentions.swift
@@ -0,0 +1,189 @@
+//
+//  Extensions.swift
+//  youtube
+//
+//  Created by Brian Voong on 6/3/16.
+//  Copyright © 2016 letsbuildthatapp. All rights reserved.
+//
+
+import UIKit
+extension UIColor {
+    static func rgb(_ red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
+extension NSMutableData {
+    
+    func appendString(string: String) {
+        let data = string.data(using: String.Encoding.utf8, allowLossyConversion: true)
+        append(data!)
+    }
+}
+
+extension UserDefaults {
+    func set(image: UIImage?, forKey key: String) {
+        guard let image = image else {
+            set(nil, forKey: key)
+            return
+        }
+        set(UIImageJPEGRepresentation(image, 1.0), forKey: key)
+    }
+    func image(forKey key:String) -> UIImage? {
+        guard let data = data(forKey: key), let image = UIImage(data: data )
+            else  { return nil }
+        return image
+    }
+    func set(imageArray value: [UIImage]?, forKey key: String) {
+        guard let value = value else {
+            set(nil, forKey: key)
+            return
+        }
+        set(NSKeyedArchiver.archivedData(withRootObject: value), forKey: key)
+    }
+    func imageArray(forKey key:String) -> [UIImage]? {
+        guard  let data = data(forKey: key),
+            let imageArray = NSKeyedUnarchiver.unarchiveObject(with: data) as? [UIImage]
+            else { return nil }
+        return imageArray
+    }
+}
+extension Array {
+    public func toDictionary<Key: Hashable>(with selectKey: (Element) -> Key) -> [Key:Element] {
+        var dict = [Key:Element]()
+        for element in self {
+            dict[selectKey(element)] = element
+        }
+        return dict
+    }
+}
+
+extension UIView {
+    open func generateOuterShadow() {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = layer.cornerRadius
+        view.layer.shadowRadius = layer.shadowRadius
+        view.layer.shadowOpacity = layer.shadowOpacity
+        view.layer.shadowColor = layer.shadowColor
+        view.layer.shadowOffset = CGSize.zero
+        view.clipsToBounds = false
+        view.backgroundColor = self.backgroundColor
+     
+        superview?.insertSubview(view, belowSubview: self)
+        
+        let constraints = [
+            NSLayoutConstraint(item: view, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 0.0),
+            NSLayoutConstraint(item: view, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1.0, constant: 0.0),
+            NSLayoutConstraint(item: view, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 0.0),
+            NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0.0),
+            ]
+        superview?.addConstraints(constraints)
+    }
+    
+  }
+    
+
+
+
+
+let imageCache = NSCache<NSString, UIImage>()
+
+class CustomImageView: UIImageView {
+    
+    var imageUrlString: String?
+    
+    func loadImageUsingUrlString(_ urlString: String,completion: @escaping (UIImage) -> ()) {
+        
+        imageUrlString = urlString
+        
+        let url = URL(string: urlString)
+        
+        image = nil
+        
+        if let imageFromCache = imageCache.object(forKey: urlString as NSString) {
+            self.image = imageFromCache
+          
+        }
+        
+        URLSession.shared.dataTask(with: url!, completionHandler: { (data, respones, error) in
+            
+            if error != nil {
+                print(error)
+                return
+            }
+            
+            
+            DispatchQueue.main.async(execute: {
+                
+                if let imageToCache =  UIImage(data: data!) {
+                    
+                    if self.imageUrlString == urlString {
+                        
+                        self.image = imageToCache
+                        imageCache.setObject(imageToCache, forKey: urlString as NSString)
+                        completion(self.image!)
+                    }
+                }
+            })
+            
+        }).resume()
+        
+       
+    }
+    
+}
+extension Double {
+    /// Rounds the double to decimal places value
+    func roundTo(places:Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+}
+
+extension Array where Element: Equatable {
+    
+    public func uniq() -> [Element] {
+        var arrayCopy = self
+        arrayCopy.uniqInPlace()
+        return arrayCopy
+    }
+    
+    mutating public func uniqInPlace() {
+        var seen = [Element]()
+        var index = 0
+        for element in self {
+            if seen.contains(element) {
+                remove(at: index)
+            } else {
+                seen.append(element)
+                index += 1
+            }
+        }
+    }
+}
+
+//extension Double{
+//
+//    var roundedTwoDigit:Double  mutating {
+//
+//        return Double(round(100*self)/100)
+//
+//    }
+//}
+extension UIView {
+    func addBackground() {
+        // screen width and height:
+        let width = UIScreen.main.bounds.size.width
+        let height = UIScreen.main.bounds.size.height
+        
+        
+        let imageViewBackground = UIImageView(frame: UIScreen.main.bounds)
+        imageViewBackground.image = UIImage(named: "background")
+        
+        // you can change the content mode:
+        imageViewBackground.contentMode = UIViewContentMode.scaleAspectFill
+        
+        self.addSubview(imageViewBackground)
+        self.sendSubview(toBack: imageViewBackground)
+    }}
+
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Parshot' do
+
+use_frameworks!
+
+# Pods for Boshra
+
+pod 'DKImagePickerController', '3.4.0'
+pod 'RichEditorView'
+pod 'ChromaColorPicker'
+end
